bdf317c9fb7cbdd713e0a605327370adabd5daef
Michael Bryan <michaelfbryan@gmail.com>
2019-07-11 05:41:52 +0000

    Implemented `Default` for `OperatingSystemClock`

--- src/lib.rs
+++ src/lib.rs
@@ -170,9 +170,9 @@ where
 
 /// A monotonically non-decreasing clock backed by the operating system.
 #[cfg(feature = "std")]
-#[derive(Debug, Default, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct OperatingSystemClock {
-    created: std::time::Instant,
+    created_at: std::time::Instant,
 }
 
 #[cfg(feature = "std")]
@@ -186,5 +186,12 @@ impl OperatingSystemClock {
 impl SystemClock for OperatingSystemClock {
     fn elapsed(&self) -> Duration {
         self.created_at.elapsed()
+    }
+}
+
+#[cfg(feature = "std")]
+impl Default for OperatingSystemClock {
+    fn default() -> OperatingSystemClock {
+        OperatingSystemClock { created_at: std::time::Instant::now() }
     }
 }
